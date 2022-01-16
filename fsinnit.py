3eef9f435b42e00d40fa267d3afe8da8b5cd743a
chocoearly44 <chocoearly44@gmail.com>
2022-01-16 20:19:52 +0100

    Update fsinnit.py

--- fsinnit.py
+++ fsinnit.py
@@ -5,6 +5,5 @@ from objects.Folder import Folder
 root = Folder("root")
 root.addFolder(Folder("td"))
 root.getFolders()["td"].addFolder(Folder("sub2"))
-root.getFolders()["td"].addFile(File("testfile", "manifestuuid", "manifestfilename", "subfilesjson"))
 
 print(jsonpickle.encode(root))
